fab12961fbca11808e829ef5e8100fbd11f0fad5
dianaaugusta <joaolimah406@gmail.com>
2023-08-26 15:00:24 -0300

    REFACTOR removing logs onCreate

--- app/src/main/java/com/example/pdflayerapiapp/MainActivity.java
+++ app/src/main/java/com/example/pdflayerapiapp/MainActivity.java
@@ -53,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "onCreate MainActivity", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "onCreate MainActivity");
         button = findViewById(R.id.searchButton);
         editText = findViewById(R.id.searchEditText);
